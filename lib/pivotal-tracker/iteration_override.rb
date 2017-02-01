9cd092b10280d73936f9a6a655c1215b51f8425f
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-01 16:52:01 -0700

    Alphabetize iteration override attributes
    [#138828481]

--- lib/pivotal-tracker/iteration_override.rb
+++ lib/pivotal-tracker/iteration_override.rb
@@ -1,6 +1,6 @@
 class PivotalTracker::IterationOverride
 
-  attr_accessor :number, :project_id, :length, :team_strength, :kind
+  attr_accessor :kind, :length, :number, :project_id, :team_strength
 
   def initialize(attributes)
     attributes.each do |attribute, value|
