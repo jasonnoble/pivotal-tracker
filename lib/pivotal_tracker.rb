2d4644db7c7e3b86d74a02dc5ec0fd16c57e0779
Mike Schutte <tmikeschutte@gmail.com>
2017-02-01 12:40:20 -0700

    Add JiraIntegration resource and unit test
    Merge conflicts
    
    [#138828505]

--- lib/pivotal_tracker.rb
+++ lib/pivotal_tracker.rb
@@ -11,6 +11,7 @@ PivotalTracker.autoload :Account,           'pivotal-tracker/account'
 PivotalTracker.autoload :AccountMembership, 'pivotal-tracker/account_membership'
 PivotalTracker.autoload :ProjectWorkspace,  'pivotal-tracker/project_workspace'
 PivotalTracker.autoload :Resource,          'pivotal-tracker/resource'
+PivotalTracker.autoload :JiraIntegration, 'pivotal-tracker/jira_integration'
 
 class PivotalTracker
   include HTTParty
