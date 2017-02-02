86848e6ea2d3d90c4c99c9b2125edadbf9be6426
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-02 16:28:15 -0700

    Add factory for notification
    [#138939699]

--- spec/lib/pivotal-tracker/notification_spec.rb
+++ spec/lib/pivotal-tracker/notification_spec.rb
@@ -1,30 +1,9 @@
 require 'spec_helper'
 
 describe PivotalTracker::Notification do
-  describe '#new' do
-    it 'initializes with valid attributes' do
-      attributes = {id: 0,
-                    project_id: 0,
-                    performer_id: 0,
-                    message: "message",
-                    context: "context",
-                    notification_type: "story",
-                    new_attachment_count: 1,
-                    action: "string",
-                    story_id: 0,
-                    epic_id: 0,
-                    comment_id: 0,
-                    created_at: DateTime.now,
-                    updated_at: DateTime.now,
-                    read_at: DateTime.now,
-                    kind: "kind"
-                    }
-
-      notification = PivotalTracker::Notification.new(attributes)
-
-      attributes.each do |attribute, value|
-        expect(notification.send(attribute)).to eq(value)
-      end
-    end
+  it_behaves_like "an API backed model" do
+    let(:attributes) { FactoryGirl.attributes_for(:notification) }
+    let(:subject) { PivotalTracker::Notification.new(attributes) }
+    let(:model) { PivotalTracker::Notification }
   end
 end
