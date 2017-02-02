b61f92b07add6c7e51499360d600c007d7aaabe3
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-02 15:50:29 -0700

    Update project workspace factory
    [#138939699]

--- spec/factories/project_workspace.rb
+++ spec/factories/project_workspace.rb
@@ -1,11 +1,11 @@
-require './lib/pivotal_tracker'
+require './lib/pivotal-tracker/project_workspace'
 
 FactoryGirl.define do
   factory :project_workspace, class: PivotalTracker::ProjectWorkspace do
-    id 1
+    id           1
     position_int 1
-    project_id 1
+    project_id   1
     workspace_id 1
-    kind 'kind'
+    kind        'project_workspace'
   end
 end
