71cea72b88d3a258b5e7234d1af4eada8b56a2fd
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-02 09:51:38 -0700

    Remove initialize from membership_summary
    [#138827905]

--- lib/pivotal-tracker/membership_summary.rb
+++ lib/pivotal-tracker/membership_summary.rb
@@ -1,10 +1,4 @@
-class PivotalTracker::MembershipSummary
+class PivotalTracker::MembershipSummary < PivotalTracker::Resource
   attr_accessor :favorite, :id, :kind, :last_viewed_at, :project_color,
                 :project_id, :project_name, :role
-
-  def initialize(attributes)
-    attributes.each do |attribute, value|
-      self.send(:"#{attribute}=", value)
-    end
-  end
 end
