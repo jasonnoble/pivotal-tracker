4fc2cb1b4ccbeae344ce0e7befe41fca5a995fa0
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-02 16:33:24 -0700

    Add factory for other_integration
    [#138939699]

--- spec/lib/pivotal-tracker/other_integration_spec.rb
+++ spec/lib/pivotal-tracker/other_integration_spec.rb
@@ -1,29 +1,9 @@
 require 'spec_helper'
 
 describe PivotalTracker::OtherIntegration do
-  describe '#new' do
-    it 'initializes with valid attributes' do
-      attributes = {id: 0,
-                    project_id: 0,
-                    can_import: true,
-                    base_url: "/",
-                    is_other: true,
-                    story_name: "story",
-                    name: "name",
-                    active: true,
-                    created_at: DateTime.now,
-                    updated_at: DateTime.now,
-                    import_api_url: "/",
-                    basic_auth_username: "username",
-                    basic_auth_password: "password",
-                    kind: "kind"
-                    }
-
-      other_integration = PivotalTracker::OtherIntegration.new(attributes)
-
-      attributes.each do |attribute, value|
-        expect(other_integration.send(attribute)).to eq(value)
-      end
-    end
+  it_behaves_like "an API backed model" do
+    let(:attributes) { FactoryGirl.attributes_for(:other_integration) }
+    let(:subject) { PivotalTracker::OtherIntegration.new(attributes) }
+    let(:model) { PivotalTracker::OtherIntegration }
   end
 end
