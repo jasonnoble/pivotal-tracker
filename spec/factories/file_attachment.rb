cb99682791eef0e310d733a870c2511db9c14703
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-02 15:23:27 -0700

    File attachment factory
    -Change file attachment spec to general api backed model form
    -Create file attachment factory
    
    [#138939699]

---
+++ spec/factories/file_attachment.rb
@@ -0,0 +1,20 @@
+require './lib/pivotal-tracker/file_attachment'
+
+FactoryGirl.define do
+  factory :file_attachment, class: PivotalTracker::FileAttachment do
+    id 1
+    filename 'filename'
+    created_at DateTime.now
+    uploader_id 1
+    thumbnailable true
+    height 1
+    width 1
+    size 1
+    download_url { Faker::Internet.url }
+    content_type 'content_type'
+    uploaded true
+    big_url { Faker::Internet.url }
+    thumbnail_url { Faker::Internet.url }
+    kind 'file_attachment'
+  end
+end
--- spec/lib/pivotal-tracker/file_attachment_spec.rb
+++ spec/lib/pivotal-tracker/file_attachment_spec.rb
@@ -1,20 +1,9 @@
 require 'spec_helper'
 
 describe PivotalTracker::FileAttachment do
-  context '#new' do
-    it 'initializes with attributes' do
-      attributes = {id: 1, filename: 'name', created_at: DateTime.now,
-        uploader_id: 1, thumbnailable: true, height: 1, width: 1, size: 1,
-        download_url: 'download', content_type: 'type', uploaded: true,
-        big_url: 'url', thumbnail_url: 'url', kind: 'kind'}
-        
-      file_attachment = PivotalTracker::FileAttachment.new(attributes)
-
-      expect(file_attachment).to be_a PivotalTracker::FileAttachment
-
-      attributes.each do |attribute, value|
-        expect(file_attachment.send(attribute)).to eq value
-      end
-    end
+  it_behaves_like "an API backed model" do
+    let(:attributes) { FactoryGirl.attributes_for(:file_attachment) }
+    let(:subject) { PivotalTracker::FileAttachment.new(attributes) }
+    let(:model) { PivotalTracker::FileAttachment }
   end
 end
