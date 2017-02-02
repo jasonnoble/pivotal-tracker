21f9afc25e9c4b95b1a2ee86faa1ab34da51eacf
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-02 16:55:19 -0700

    Align model attributes
    [#138939699]

--- spec/factories/google_attachment.rb
+++ spec/factories/google_attachment.rb
@@ -2,14 +2,14 @@ require './lib/pivotal-tracker/google_attachment'
 
 FactoryGirl.define do
   factory :google_attachment, class: PivotalTracker::GoogleAttachment do
-    id    1
-    comment_id 1
-    person_id  1
-    google_kind 'string'
-    title 'string'
-    google_id 'string'
+    id              1
+    comment_id      1
+    person_id       1
+    google_kind    'string'
+    title          'string'
+    google_id      'string'
     alternate_link 'string'
-    resource_id 'string'
-    kind 'string'
+    resource_id    'string'
+    kind           'string'
   end
 end
--- spec/factories/me.rb
+++ spec/factories/me.rb
@@ -2,20 +2,20 @@ require './lib/pivotal-tracker/me'
 
 FactoryGirl.define do
   factory :me, class: PivotalTracker::Me do
-    api_token '0'
-    created_at DateTime.now
-    email { Faker::Internet.email(name) }
-    has_google_identity true
-    id 1
-    initials { name.split.map{|name| name[0] } }
-    kind "me"
-    name "username"
-    personal_settings ({})
-    project_ids ["#{ENV['PROJECT_ID']}"]
+    api_token                     "0"
+    created_at                    DateTime.now
+    email                         { Faker::Internet.email(name) }
+    has_google_identity           true
+    id                            1
+    initials                      { name.split.map{|name| name[0] } }
+    kind                          "me"
+    name                          { Faker::Name.name }
+    personal_settings             ({})
+    project_ids                   ["#{ENV['PROJECT_ID']}"]
     receives_in_app_notifications true
-    time_zone Time.now.zone
-    updated_at DateTime.now
-    username "username"
-    workspace_ids [1,2]
+    time_zone                     { Time.zone.now }
+    updated_at                    DateTime.now
+    username                      { Faker::Internet.user_name }
+    workspace_ids                 [1,2]
   end
 end
