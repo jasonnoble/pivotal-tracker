e63cc440160eda581746f3f1b303d8cc7aa84b6b
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-01 21:47:44 -0700

    Remove initialize method and inherit from resource
    [#138828467]

--- lib/pivotal-tracker/iteration.rb
+++ lib/pivotal-tracker/iteration.rb
@@ -1,13 +1,10 @@
-class PivotalTracker::Iteration
+require './lib/pivotal-tracker/resource'
+
+class PivotalTracker::Iteration < PivotalTracker::Resource
 
   attr_accessor :accepted, :accepted_points, :analytics, :created,
     :effective_points, :finish, :kind, :length, :number,
     :points, :project_id, :start, :story_ids, :team_strength,
     :velocity
 
-  def initialize(attributes)
-    attributes.each do |attribute, value|
-      self.send(:"#{attribute}=", value)
-    end
-  end
 end
