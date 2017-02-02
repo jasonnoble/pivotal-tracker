7938362aae3dfae2bd951a887f7aa8814ba3f8fd
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-02 15:31:06 -0700

    Update iteration factory
    [#138939699]

--- spec/factories/iteration.rb
+++ spec/factories/iteration.rb
@@ -1,21 +1,21 @@
-require './lib/pivotal_tracker'
+require './lib/pivotal-tracker/iteration'
 
 FactoryGirl.define do
   factory :iteration, class: PivotalTracker::Iteration do
-    number 1
-    project_id 1
-    length 2
-    team_strength 1.0
-    story_ids []
-    start Time.now
-    finish Time.now
-    velocity 1.0
-    points 1
-    accepted_points 1
+    number           1
+    project_id       1
+    length           2
+    team_strength    1.0
+    story_ids        []
+    start            DateTime.now
+    finish           DateTime.now
+    velocity         1.0
+    points           1
+    accepted_points  1
     effective_points 1
-    accepted []
-    created []
-    analytics []
-    kind 'kind'
+    accepted         []
+    created          []
+    analytics        []
+    kind             'iteration'
   end
 end
