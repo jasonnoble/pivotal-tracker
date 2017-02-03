19eee5b75c348ca9cfb614044bf36378b8d4f9ee
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-02 17:12:09 -0700

    Add factory for get_satisfaction_integration
    [#138939699]

--- spec/lib/pivotal-tracker/get_satisfaction_integration_spec.rb
+++ spec/lib/pivotal-tracker/get_satisfaction_integration_spec.rb
@@ -1,25 +1,9 @@
 require 'spec_helper'
 
 describe PivotalTracker::GetSatisfactionIntegration do
-  # We will be testing the class, not the instance
-  subject { PivotalTracker::GetSatisfactionIntegration }
-
-  describe ".new" do
-    it "can initialize an instance with attributes" do
-      attributes = {
-        id: 123, project_id: 234, can_import: true,
-        base_url: 'url', is_other: true, story_name: 'name',
-        name: 'name', active: true, created_at: Time.now,
-        updated_at: Time.now, company: 'company',
-        product: 'thing', kind: 'thing'
-      }
-
-      get_satisfaction_integration = subject.new(attributes)
-      expect(get_satisfaction_integration).to be_a PivotalTracker::GetSatisfactionIntegration
-
-      attributes.each do |attribute, value|
-        expect(get_satisfaction_integration.send(attribute)).to eq(value)
-      end
-    end
+  it_behaves_like "an API backed model" do
+    let(:attributes) { FactoryGirl.attributes_for(:get_satisfaction_integration) }
+    let(:model) { PivotalTracker::GetSatisfactionIntegration }
+    let(:subject) { model.new(attributes) }
   end
 end
